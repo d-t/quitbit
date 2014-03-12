f938bd3048c272d11a549d76fe0dda5131251053
Davide Totaro <ToT@davide-totaros-macbook.local>
2014-03-13 00:30:05 +0800

    wsgi.py updated - dj-static

--- quitbit/wsgi.py
+++ quitbit/wsgi.py
@@ -23,7 +23,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quitbit.settings.dev")
 # import django.core.handlers.wsgi
 # application = django.core.handlers.wsgi.WSGIHandler()
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
